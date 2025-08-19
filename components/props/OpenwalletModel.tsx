53f1c954fb04f2ee060b1267244a8152f452f00d
Kollusion <kollusion.dark@gmail.com>
2025-08-19 19:25:56 +0700

    переход

--- components/props/OpenwalletModel.tsx
+++ components/props/OpenwalletModel.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import * as React from 'react';
-import { useRouter } from 'next/navigation';
-import { useLocale, useTranslations } from 'next-intl';
+import React, { useState } from 'react';
+import { useTranslations } from 'next-intl';
 import { Wallet } from 'lucide-react';
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
@@ -11,34 +10,42 @@ import { useOpenWalletModal } from '@/store/modalStore';
 
 export default function WalletModal() {
     const t = useTranslations('Modal.Openwallet');
-    const d = useTranslations('Default');
+    const [open, setOpen] = useState<boolean>(false)
 
     const { isOpen, close } = useOpenWalletModal();
-    const router = useRouter();
-    const locale = useLocale();
+
+    const handleOpen = () => {
+        setOpen(true);
+        close();
+    }
 
     return (
-        <Dialog open={isOpen} onOpenChange={(open) => { if (!open) close(); }}>
-            <DialogContent className="sm:max-w-[28rem] w-full">
-                <DialogHeader>
-                    <DialogTitle>{t('title')}</DialogTitle>
-                    <DialogDescription>
-                        {t('subtitle')}
-                    </DialogDescription>
-                </DialogHeader>
-                <div className='flex flex-col items-center gap-6'>
-                    <div>
-                        <div className='w-64 h-64 bg-accent animate-pulse rounded-xl'></div>
-                        <p className='text-xs mt-2 text-center'>{t('downloadMobile')}</p>
+        <>
+            <div className={`fixed inset-0 w-full h-screen bg-background flex justify-center items-center z-99 duration-500 ${open? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+                <p className='font-mono text-4xl'>Zyphera Wallet</p>
+            </div>
+            <Dialog open={isOpen} onOpenChange={(open) => { if (!open) close(); }}>
+                <DialogContent className="sm:max-w-[28rem] w-full">
+                    <DialogHeader>
+                        <DialogTitle>{t('title')}</DialogTitle>
+                        <DialogDescription>
+                            {t('subtitle')}
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className='flex flex-col items-center gap-6'>
+                        <div>
+                            <div className='w-64 h-64 bg-accent animate-pulse rounded-xl'></div>
+                            <p className='text-xs mt-2 text-center'>{t('downloadMobile')}</p>
+                        </div>
+                        <div className='flex items-center justify-center gap-2 w-full'>
+                            <div className='w-1/3 border'></div>
+                            <p className='text-muted-foreground'>{t('or')}</p>
+                            <div className='w-1/3 border'></div>
+                        </div>
+                        <Button size={'xl'} onClick={() => { handleOpen() }}><Wallet />{t('openWeb')}</Button>
                     </div>
-                    <div className='flex items-center justify-center gap-2 w-full'>
-                        <div className='w-1/3 border'></div>
-                        <p className='text-muted-foreground'>{t('or')}</p>
-                        <div className='w-1/3 border'></div>
-                    </div>
-                    <Button size={'xl'}><Wallet />{t('openWeb')}</Button>
-                </div>
-            </DialogContent>
-        </Dialog>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
